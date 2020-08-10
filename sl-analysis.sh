ad07bd8d24e576412cabf2e31a8eb41965b2eccd
Alok Shukla <32108973+alokshukla1978@users.noreply.github.com>
2020-08-10 11:28:54 -0700

    Update sl-analysis.sh

--- sl-analysis.sh
+++ sl-analysis.sh
@@ -18,13 +18,25 @@ sl analyze --version-id "$GITHUB_SHA" --tag branch="$GITHUB_BRANCH" --app "$GITH
 URL="https://www.shiftleft.io/violationlist/$GITHUB_PROJECT?apps=$GITHUB_PROJECT&isApp=1"
 BUILDRULECHECK=$(sl check-analysis --app "$GITHUB_PROJECT" --branch "$GITHUB_BRANCH")
 
+# Set up comment body for the merge request
+COMMENT_BODY='{"raw":""}'
+COMMENT_BODY=$(echo "$COMMENT_BODY" | jq '.raw += "## NG-SAST Analysis Findings \n "')
+
+NEW_FINDINGS=$(curl -H "Authorization: Bearer $SHIFTLEFT_API_TOKEN" "https://www.shiftleft.io/api/v4/orgs/$SHIFTLEFT_ORG_ID/apps/$APP_NAME/scans/compare?source=tag.branch=$BITBUCKET_BRANCH&target=tag.branch=$BITBUCKET_BRANCH" | jq -c -r '.response.common | .? | .[] | "* [ID " + .id + "](https://www.shiftleft.io/findingDetail/" + .app + "/" + .id + "): " + "["+.severity+"] " + .title')
+
+echo $NEW_FINDINGS
+
+COMMENT_BODY=$(echo "$COMMENT_BODY" | jq ".raw += \"### New findings \n  \n \"")
+COMMENT_BODY=$(echo "$COMMENT_BODY" | jq ".raw += \"$NEW_FINDINGS \n  \n \"")
+
+echo "COMMENT_BODY: $COMMENT_BODY"
 if [ -n "$BUILDRULECHECK" ]; then
     PR_COMMENT="Build rule failed, click here for vulnerability list! - $URL"  
     echo $PR_COMMENT
     curl -XPOST "https://api.github.com/repos/$GITHUB_REPO/issues/$PULL_REQUEST/comments" \
       -H "Authorization: Bearer $GITHUB_TOKEN" \
       -H "Content-Type: application/json" \
-      -d "{\"body\": \"$PR_COMMENT\"}"
+      -d "{\"body\": \"$COMMENT_BODY\"}"
     exit 1
 else
     PR_COMMENT="Build rule succeeded, click here for vulnerability list! - $URL" 
@@ -32,6 +44,6 @@ else
     curl -XPOST "https://api.github.com/repos/$GITHUB_REPO/issues/$PULL_REQUEST/comments" \
       -H "Authorization: Bearer $GITHUB_TOKEN" \
       -H "Content-Type: application/json" \
-      -d "{\"body\": \"$PR_COMMENT\"}"
+      -d "{\"body\": \"$COMMENT_BODY\"}"
     exit 0
 fi

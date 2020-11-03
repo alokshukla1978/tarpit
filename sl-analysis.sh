69e2a48363bd7c863e2459ff1fab4b59013499a2
Alok Shukla <32108973+alokshukla1978@users.noreply.github.com>
2020-11-03 06:00:34 -0800

    Update sl-analysis.sh

--- sl-analysis.sh
+++ sl-analysis.sh
@@ -17,7 +17,7 @@ sl analyze --version-id "$GITHUB_SHA" --tag branch="$GITHUB_BRANCH" --app "$GITH
 # Run Build rule check!  
 URL="https://www.shiftleft.io/violationlist/$GITHUB_PROJECT?apps=$GITHUB_PROJECT&isApp=1"
 #BUILDRULECHECK=$(sl check-analysis --app "$GITHUB_PROJECT" --branch "$GITHUB_BRANCH")
-sl check-analysis --app "$GITHUB_PROJECT" --branch "$GITHUB_BRANCH" --report --github-pr-number=${{github.event.number}} --github-pr-user=${{ github.repository_owner }} --github-pr-repo=${{ github.event.repository.name }}  --github-token=${{ secrets.GITHUB_TOKEN }}
+sl check-analysis --app "$GITHUB_PROJECT" --branch "$GITHUB_BRANCH" --report --github-pr-number="$1" --github-pr-user="$2" --github-pr-repo="$3" --github-token="$4"
 
 # if [ -n "$BUILDRULECHECK" ]; then
 #    PR_COMMENT="Build rule failed, click here for vulnerability list! - $URL"  
